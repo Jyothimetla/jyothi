44c4e45fb61805fb3c20cb3059d4566de5b9de38
jyothime <jyothime@VMT13029.VALUEMOMENTUM.BIZ>
2023-01-19 02:36:58 -0800

    second commit

---
+++ Jyothihub/src/assignment/testing.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class testing {
+
+	public static void main(String[] args) {
+		int[] ar= {1,2,3};
+		int[] ar1= {4,5,6};
+		if(ar.equals(ar1))
+		    System.out.println("contains same elements");
+		else
+		    System.out.println("diff elements");
+		    
+	}
+
+}
